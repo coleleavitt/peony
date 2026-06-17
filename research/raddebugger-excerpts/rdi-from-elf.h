406c9387ffb75dee26ebe6def0e4e2c27912fd34
Cole Leavitt <cole@unwrap.rs>
2026-06-16 23:45:41 -0700

    chore: add .gitignore exclusions, switch ws-deque to git dep, untrack jfc/research artifacts

--- research/raddebugger-excerpts/rdi-from-elf.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Epic Games Tools
-// Licensed under the MIT license (https://opensource.org/license/mit/)
-
-#ifndef RDI_FROM_ELF_H
-#define RDI_FROM_ELF_H
-
-internal RDIM_BinarySectionList e2r_rdi_binary_sections_from_elf_section_table(Arena *arena, String8 data, ELF_Bin *bin, ELF_Shdr64Array *shdrs);
-
-#endif // RDI_FROM_ELF_H
